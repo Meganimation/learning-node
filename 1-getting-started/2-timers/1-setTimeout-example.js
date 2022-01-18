4e4646a52cc46e4756bb786f60321cdfc3e73d86
Adams <megan.adams@capgemini.com>
2022-01-18 10:45:57 -0500

    setting timeouts

--- 1-getting-started/2-timers/1-setTimeout-example.js
+++ 1-getting-started/2-timers/1-setTimeout-example.js
@@ -1,6 +1,29 @@
-setTimeout(
-  () => {
-    console.log('Hello after 4 seconds');
-  },
-  4 * 1000
-);
+
+
+function setMultiTimeout(callback, ...args) {
+  setTimeout(
+    () => {
+      console.log('hi after 4 seconds, btw im the setMultiTimeout function')
+    },
+    4 * 1000,
+  );
+
+  setTimeout(
+    () => {
+      console.log('hi after 8, btw im the setMultiTimeout functio')
+    },
+    8 * 1000,
+  );
+
+}
+
+const theOneFunc=(delay, name='default')=>{
+  console.log(`Hi to ${name} after ${delay} seconds`);
+}
+
+setTimeout(theOneFunc, 4*1000 ,4, 'megan')
+setTimeout(theOneFunc, 8*1000, 8)
+
+
+// setMultiTimeout()
+
